68a3d41f2d2d32d872d92c9cbbf65d1bde7d1599
zyansheep <zyansheep@github.com>
2021-12-15 18:49:26 -0500

    get beta reduction working with not function

---
+++ src/lambda_calculus.rs
@@ -0,0 +1,230 @@
+use std::fmt;
+
+use lazy_static::lazy_static;
+use thiserror::Error;
+use bitvec::prelude::*;
+use bytes::{Buf, BufMut};
+
+use hashdb::{Data, Datastore, Hash};
+
+lazy_static!{
+	pub static ref VARIABLE:     Hash = Hash::from_sha256_digest([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+	pub static ref LAMBDA:      Hash = Hash::from_sha256_digest([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+	pub static ref APPLICATION: Hash = Hash::from_sha256_digest([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2]);
+}
+
+#[derive(Error, Debug)]
+pub enum LambdaError {
+	#[error("Invalid hashtype for Lambda type. got {0} expected {}", LAMBDA.clone())]
+	InvalidLayoutForLambda(Hash),
+	#[error("Invalid hashtype for Lambda type. got {0} expected {}", APPLICATION.clone())]
+	InvalidLayoutForApplication(Hash),
+	#[error("Invalid hashtype, got {0}")]
+	InvalidLayout(Hash),
+	#[error("Can't convert Variable variant to data, there is no data to serialize")]
+	NoDataForVariable,
+	#[error("Recursion Depth for beta reduction exceeded")]
+	RecursionDepthExceeded,
+	
+	#[error("Not in datastore: {0}")]
+	NotInDatastore(Hash),
+
+	#[error("Error in bincode")]
+	SerdeError(#[from] bincode::Error),
+}
+
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum Expr {
+	Variable,
+	Lambda(Hash, Vec<BitVec>),
+	Application(Hash, Hash),
+}
+
+impl Expr {
+	pub fn new_lambda(pointers: Vec<BitVec>, expr: Hash) -> Self {
+		Expr::Lambda(expr, pointers)
+	}
+	pub fn new_application(lambda: Hash, expr: Hash) -> Self {
+		Expr::Application(lambda, expr)
+	}
+	pub fn to_hash(&self, db: &mut Datastore) -> Hash {
+		match self {
+			Expr::Variable => VARIABLE.clone(),
+			_ => db.add(self.to_data().unwrap())
+		}
+	}
+	pub fn to_data(&self) -> Result<Data, LambdaError> {
+		match self {
+			Expr::Variable => Err(LambdaError::NoDataForVariable),
+			Expr::Lambda(expr, pointers) => {
+				let mut buf = Vec::new();
+				buf.put(LAMBDA.as_bytes());
+				buf.put(expr.as_bytes());
+				
+				bincode::serialize_into(&mut buf, pointers)?;
+			
+				Ok(Data::from_vec(buf))
+			}
+			Expr::Application(lambda, expr) => {
+				let mut buf = Vec::new();
+				buf.put(APPLICATION.as_bytes());
+				buf.put(lambda.as_bytes());
+				buf.put(expr.as_bytes());
+				Ok(Data::from_vec(buf))
+			}
+		}
+	}
+	pub fn from_hash(hash: &Hash, db: &mut Datastore) -> Result<Self, LambdaError> {
+		if *hash == *VARIABLE { Ok(Expr::Variable) }
+		else { Self::from_data( db.get(&hash).ok_or(LambdaError::NotInDatastore(hash.clone()))? ) }
+	}
+	pub fn from_data(data: &Data) -> Result<Self, LambdaError> {
+		let mut reader = data.as_bytes();
+		let layout = &reader.copy_to_bytes(LAMBDA.len())[..];
+		Ok(match layout {
+			_ if layout == LAMBDA.as_bytes() => {
+				let expr = Hash::from_raw_bytes(&reader.copy_to_bytes(LAMBDA.len())[..]);
+				let pointers = bincode::deserialize_from(reader)?;
+				
+				Expr::Lambda(expr, pointers)
+			},
+			_ if layout == APPLICATION.as_bytes() => {
+				Expr::Application(
+					Hash::from_raw_bytes(&reader.copy_to_bytes(LAMBDA.len())[..]),
+					Hash::from_raw_bytes(&reader.copy_to_bytes(LAMBDA.len())[..])
+				)
+			},
+			_ => Err(LambdaError::InvalidLayout(Hash::from_raw_bytes(layout)))?
+		})
+	}
+}
+
+// TODO: Make this better
+impl fmt::Display for Expr {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "{:?}", self)
+	}
+}
+pub fn print_expr(hash: &Hash, db: &mut Datastore) -> Result<(), LambdaError> {
+	Ok(println!("{}", format_expr(&Expr::from_hash(hash, db)?, db)?))
+}
+pub fn format_expr(expr: &Expr, db: &mut Datastore) -> Result<String, LambdaError> {
+	Ok(match expr {
+		Expr::Variable => "x".to_owned(),
+		Expr::Lambda(hash, pointers) => {
+			//use itertools::Itertools;
+			let bool_array = pointers.iter().map(|s|{
+				s.iter().by_val().map(|b|if b {'1'} else {'0'}).collect::<String>()
+			}).intersperse(",".to_owned()).collect::<String>();
+			format!("(λ[{}] {})", bool_array, format_expr(&Expr::from_hash(&hash, db)?, db)?)
+		},
+		Expr::Application(lambda, expr) => {
+			let lambda_string = format_expr(&Expr::from_hash(lambda, db)?, db)?;
+			let expr_string = format_expr(&Expr::from_hash(expr, db)?, db)?;
+			format!("({} {})", lambda_string, expr_string)
+		},
+	})
+}
+pub fn parse_hash(hash: &Hash, db: &mut Datastore) -> Result<String, LambdaError> {
+	format_expr(&Expr::from_hash(hash, db)?, db)
+}
+
+/// Takes lambda expression and traverses down the binary tree using bitslices, replacing variables & rehashing.
+fn recur_replace(replace_in_expr: Expr, pointers: Vec<&BitSlice<Lsb0>>, replacement: Expr, db: &mut Datastore) -> Result<Expr, LambdaError> {
+	if pointers.len() == 0 { return Ok(replace_in_expr) }
+
+	let going_left = &mut false;
+	let going_right = &mut false;
+
+	match replace_in_expr {
+		// When encounter a variable, replace
+		Expr::Variable => {
+			Ok(replacement)
+		},
+		// When encounter a lambda, skip
+		Expr::Lambda(expr, ptrs) => {
+			let pointers = pointers.into_iter().filter_map(|pointer| {
+				// Check first bit
+				*going_left |= !pointer[0];
+				*going_right |= pointer[0];
+				// If more than 1 bit left, return slice of bitslice
+				if pointer.len() > 1 {
+					Some(&pointer[1..])
+				} else { None }
+			}).collect::<Vec<&BitSlice<Lsb0>>>();
+
+			let sub_replace_in_expr = Expr::from_hash(&expr, db)?;
+			Ok(Expr::Lambda(recur_replace(sub_replace_in_expr, pointers, replacement, db)?.to_hash(db), ptrs))
+		},
+		// When encounter a function application, check if any of the bit arrays want to go down each path, then go down those paths.
+		Expr::Application(lambda, expr) => {
+			// Check if any of the bit arrays are 0 or 1
+			let new_vec = pointers.into_iter().filter_map(|pointer| {
+				// Check first bit
+				*going_left |= !pointer[0];
+				*going_right |= pointer[0];
+				// If more than 1 bit left, return slice of bitslice
+				if pointer.len() > 1 {
+					Some(&pointer[1..])
+				} else { None }
+			}).collect::<Vec<&BitSlice<Lsb0>>>();
+
+			let left_replace = if *going_left {
+				Some(recur_replace(Expr::from_hash(&lambda, db)?, new_vec.clone(), replacement.clone(), db)?.to_hash(db))
+			} else { None };
+
+			let right_replace = if *going_right {
+				Some(recur_replace(Expr::from_hash(&expr, db)?, new_vec, replacement.clone(), db)?.to_hash(db))
+			} else { None };
+
+			let lambda = left_replace.unwrap_or(lambda);
+			let expr = right_replace.unwrap_or(expr);
+
+			Ok(Expr::Application(lambda, expr))
+		}
+	}
+}
+
+// Beta reduces expression without rehashing
+pub fn partial_beta_reduce(reducing_expr: Expr, max_depth: usize, db: &mut Datastore) -> Result<Expr, LambdaError> {
+	if max_depth == 0 { return Err(LambdaError::RecursionDepthExceeded) }
+	let max_depth = max_depth - 1;
+
+	Ok(match reducing_expr {
+		Expr::Variable => reducing_expr,
+		Expr::Lambda(expr, pointers) => {
+			// IMPORTANT: Lambda exprs shouldn't be able to reduce far enough to change pointed to variables (i.e. pointers shouldn't point to children of any lambdas which are in a function application)
+			Expr::Lambda(beta_reduce(&expr, db)?, pointers)
+		}
+		Expr::Application(lambda_hash, replace_hash) => {
+			let lambda_reduced = partial_beta_reduce(Expr::from_hash(&lambda_hash, db)?, max_depth, db)?;
+			match lambda_reduced {
+				Expr::Variable => Expr::Application(VARIABLE.clone(), beta_reduce(&replace_hash, db)?),
+				Expr::Lambda(expr_hash, pointers) => {
+					let expr = Expr::from_hash(&expr_hash, db)?;
+					// Convert BitVecs to BitSlices for recur_replace
+					let pointers = pointers.iter().map(|s|s.as_bitslice()).collect::<Vec<&BitSlice<Lsb0>>>();
+					let replacement = Expr::from_hash(&replace_hash, db)?;
+
+					// Debug
+					let bool_array = pointers.iter().map(|s|{
+						s.iter().by_val().map(|b|if b {'1'} else {'0'}).collect::<String>()
+					}).intersperse(", ".to_owned()).collect::<String>();
+					print!("replace in: {} at [{}] with {}", format_expr(&expr, db)?, bool_array, format_expr(&replacement, db)?);
+					let replaced_form = recur_replace(expr, pointers, replacement, db)?;
+					println!(" = {}", format_expr(&replaced_form, db)?);
+
+					partial_beta_reduce(replaced_form, max_depth, db)?
+				},
+				Expr::Application(_, _) => {
+					Expr::Application(lambda_reduced.to_hash(db), beta_reduce(&replace_hash, db)?)
+				}
+			}
+		}
+	})
+}
+
+pub fn beta_reduce(hash: &Hash, db: &mut Datastore) -> Result<Hash, LambdaError> {
+	Ok(partial_beta_reduce(Expr::from_hash(hash, db)?, 20, db)?.to_hash(db))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,57 @@
-use std::io::Read;
+#![feature(iter_intersperse)]
+
+use std::{hash, io::Read};
+use bitvec::prelude::*;
 
 use hashdb::*;
 
+use crate::lambda_calculus::{APPLICATION, Expr, LAMBDA, VARIABLE, beta_reduce, format_expr, parse_hash, print_expr};
 
+mod lambda_calculus;
 
 fn main() {
-    let mut db = Datastore::new();
+	let db = &mut Datastore::new();
 
-    let data = Data::new("Hello, World!\n".as_bytes());
+	//let data = Data::new("Hello, World!\n".as_bytes());
+	// Identity Function
+	let id_hash = Expr::Lambda(VARIABLE.clone(), vec![bitvec![0]]).to_hash(db);
+	println!("I = {}", parse_hash(&id_hash, db).unwrap());
 
-    /* let mut buf = Vec::new();
-    std::fs::File::open("/home/zyansheep/Documents/Personal/z_key.pub").unwrap().read_to_end(&mut buf).unwrap();
-    let data = Data::new(&buf[..]); */
+	// Apply identity function to itself
+	let ii_app_hash = Expr::Application(id_hash.clone(), id_hash.clone()).to_hash(db);
+	println!("(I I) = {}", parse_hash(&ii_app_hash, db).unwrap());
 
-    println!("Have data: {:x?}", data.as_bytes());
-    let hash = db.add(data);
-    let data = db.get(&hash).unwrap().clone();
-    let hash2 = db.add(data);
-    let data = db.get(&hash2).unwrap();
-    assert_eq!(hash, hash2);
+	let ii_hash = beta_reduce(&ii_app_hash, db).unwrap();
+	println!("(I I) = I = {}", parse_hash(&ii_hash, db).unwrap());
 
-    //println!("Data: {}", std::str::from_utf8(data.as_bytes()).unwrap());
-    println!("Hash: {}", hash);
+	assert_eq!(id_hash, ii_hash);
+
+	let k_hash = Expr::Lambda(Expr::Lambda(VARIABLE.clone(), vec![]).to_hash(db), vec![bitvec![0, 0]]).to_hash(db);
+	println!("K = {}", parse_hash(&k_hash, db).unwrap());
+	
+	let ki_app_hash = Expr::Application(k_hash.clone(), id_hash.clone()).to_hash(db);
+	println!("(K I) = {}", parse_hash(&ki_app_hash, db).unwrap());
+
+	let ki_hash = beta_reduce(&ki_app_hash, db).unwrap();
+	println!("(K I) = KI = {}", parse_hash(&ki_hash, db).unwrap());
+
+	let false_hash = ki_hash.clone();
+	let true_hash = k_hash.clone();
+
+	let not_hash = Expr::Lambda(
+		Expr::Application(
+			Expr::Application(VARIABLE.clone(), false_hash.clone()).to_hash(db),
+			true_hash.clone()
+		).to_hash(db), vec![bitvec![0, 0, 0]]
+	).to_hash(db);
+	println!("(λ[000] ((x KI) K)) = Not = {}", parse_hash(&not_hash, db).unwrap());
+	let reduced_not_hash = beta_reduce(&not_hash, db).unwrap();
+	assert_eq!(not_hash, reduced_not_hash);
+
+	let not_true_app_hash = Expr::Application(not_hash, true_hash).to_hash(db);
+	println!("(Not True) = {}", parse_hash(&not_true_app_hash, db).unwrap());
+	let not_true_hash = beta_reduce(&not_true_app_hash, db).unwrap();
+	println!("(Not True) = {}", parse_hash(&not_true_hash, db).unwrap());
+
+	assert_eq!(not_true_hash, false_hash);
 }
