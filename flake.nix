809a2df807e8375c02f12b9efc9813d0ab7cd68f
zyansheep <zyansheep@github.com>
2022-02-04 12:33:34 -0500

    streamline nix flake config

--- flake.nix
+++ flake.nix
@@ -1,61 +1,24 @@
 {
-  	inputs = {
-		nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-		rust-overlay.url = "github:oxalica/rust-overlay";
-		utils.url = "github:numtide/flake-utils";
-  	};
-
-  	outputs = { self, nixpkgs, utils, rust-overlay, ... }:
-	utils.lib.eachDefaultSystem (system: let
-		overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-        	inherit system overlays;
-        };
-		rust-toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-			extensions = [ "rust-src" "clippy" ];
-		});
-
-		llvmPkgs = pkgs.buildPackages.llvmPackages_11;
-		mkShell = pkgs.mkShell.override { stdenv = llvmPkgs.stdenv; };
-	in rec {
-		# `nix develop`
-		devShell = mkShell {
-			nativeBuildInputs = with pkgs; [
-				pkg-config
-				cmake
-				rust-toolchain
-				cargo-edit
-				gdb
-				lldb
-				vscode-extensions.vadimcn.vscode-lldb
-
-				firejail
-				simplenes
-			];
-			buildInputs = with pkgs; [
-				llvmPkgs.bintools
-				#stdenv.cc.cc.lib
-				#lld
-				
-				x11
-				xorg.libXcursor
-				xorg.libXrandr
-				xorg.libXi
-				libxkbcommon
-				vulkan-tools
-				vulkan-headers
-				vulkan-loader
-				vulkan-validation-layers
-				fontconfig
-				freetype
-				protobuf
-			];
-			hardeningDisable = [ "fortify" ];
-			NIX_CFLAGS_LINK = "-fuse-ld=lld";
-			#LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH"; # Fix can't find libstdc++.so.6
-			#PKG_CONFIG_PATH = "${pkgs.libxkbcommon.dev}/lib/pkgconfig";
-			
-			LD_LIBRARY_PATH="${pkgs.vulkan-loader}/lib"; # Vulkan Fix
+	inputs = {
+		nixCargoIntegration.url = "github:yusdacra/nix-cargo-integration";
+	};
+	outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs {
+		root = ./.;
+		overrides = {
+			shell = common: prev: with common.pkgs; {
+				# hardeningDisable = [ "fortify" ];
+				env = prev.env ++ [
+					{
+						# Vulkan doesn't work without this for some reason :(
+						name = "LD_LIBRARY_PATH";
+						eval = "$LD_LIBRARY_PATH${pkgs.vulkan-loader}/lib";
+					}
+					{
+						name = "NIX_CFLAGS_LINK";
+						eval = "-fuse-ld=lld";
+					}
+				];
+			};
 		};
-	});
+	};
 }
