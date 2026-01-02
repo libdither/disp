c0ebe35c5f4e53a1f57dd432100b9498ebc3b9b9
zyansheep <zyansheep@protonmail.com>
2026-01-02 16:51:05 -0500

    new project init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<main>
+    <h1>Disp</h1>
+    <p>Code Editor & Compiler</p>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+    }
+</style>
